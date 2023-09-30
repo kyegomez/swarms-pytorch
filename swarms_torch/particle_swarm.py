770c664d0657bd264962d237172507df1e718f27
Kye <kye@apacmediasolutions.com>
2023-09-30 10:19:53 -0400

    cleanup

--- swarms_torch/particle_swarm.py
+++ swarms_torch/particle_swarm.py
@@ -50,40 +50,54 @@ class ParticleSwarmOptimization:
     
     """
     def __init__(
-        self, 
-        goal, 
-        n_particles=100, 
-        inertia=0.5, 
-        personal_best_weight=1.5, 
-        global_best_weight=1.5
+        self,
+        goal: str = None,
+        n_particles: int = 100,
+        inertia: float = 0.5,
+        personal_best_weight: float = 1.5,
+        global_best_weight: float = 1.5,
+        dim: int = 1
     ):
         self.goal = torch.tensor([ord(c) for c in goal])
         self.n_particles = n_particles
         self.inertia = inertia
         self.personal_best_weight = personal_best_weight
         self.global_best_weight = global_best_weight
-        
+
         self.particles = torch.randint(0, 255, (n_particles, len(goal)))
         self.velocities = torch.zeros((n_particles, len(goal)))
-        
+
         self.personal_best = self.particles.clone()
         self.global_best = self.particles[0].clone()
-
-    def compute_fitness(self, particle):
+    
+    def compute_fitness(
+        self,
+        particle,
+    ):
         return 1.0 / (1.0 + torch.norm((particle - self.goal).float()))
 
-    def update(self):
+    
+    def update(
+        self,
+    ):
         for i in range(self.n_particles):
-            fitness = self.compute_fitness(self.particles[i])
-            personal_best_fitness = self.compute_fitness(self.personal_best[i])
-            
+            fitness = self.compute_fitness(
+                self.particles[i],
+            )
+
+            personal_best_fitness = self.compute_fitness(
+                self.personal_best[i],
+            )
+
             if fitness > personal_best_fitness:
                 self.personal_best[i] = self.particles[i]
             
             global_best_fitness = self.compute_fitness(self.global_best)
+
             if fitness > global_best_fitness:
                 self.global_best = self.particles[i]
 
+            #update velocity
             # Update velocity
             personal_attraction = self.personal_best_weight * torch.rand(self.goal.size()) * (self.personal_best[i] - self.particles[i])
             global_attraction = self.global_best_weight * torch.rand(self.goal.size()) * (self.global_best - self.particles[i])
@@ -94,12 +108,20 @@ class ParticleSwarmOptimization:
             self.particles[i] += self.velocities[i].int()
             self.particles[i].clamp_(0, 255)
 
-    def optimize(self, iterations):
+    def optimize(
+        self,
+        iterations: int = 1000,
+    ):
         for _ in range(iterations):
             self.update()
             best_particle = self.global_best
-            print("Best Particle: ", ''.join([chr(int(i)) for i in best_particle]))
+            print(
+                "Best Particle: ",
+                ''.join([chr(int(i)) for i in best_particle])
+            )
 
-# Test
+
+
+#test
 pso = ParticleSwarmOptimization(goal="Attention is all you need", n_particles=100)
 pso.optimize(iterations=1000)
