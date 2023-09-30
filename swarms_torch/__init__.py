2b1a58677eb9c1c5673b7250d8fba43638838c61
Kye <kye@apacmediasolutions.com>
2023-09-30 19:53:46 -0400

    clean upg

--- swarms_torch/__init__.py
+++ swarms_torch/__init__.py
@@ -1,3 +1,11 @@
 from swarms_torch.particle_swarm import ParticleSwarmOptimization
 from swarms_torch.ant_colony_swarm import AntColonyOptimization
 from swarms_torch.queen_bee_swarm import QueenBeeGa
+from swarms_torch.spiral_optimization import SPO
+
+__all__ = [
+    "ParticleSwarmOptimization",
+    "AntColonyOptimization",
+    "QueenBeeGa",
+    "SPO"
+]
