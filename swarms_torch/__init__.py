acf4c6c70517e1bfd38ff97f168e0aa4e3c8fe62
Kye <kye@apacmediasolutions.com>
2023-10-04 13:40:06 -0400

    cellular swarm with transformer

--- swarms_torch/__init__.py
+++ swarms_torch/__init__.py
@@ -3,6 +3,9 @@ from swarms_torch.ant_colony_swarm import AntColonyOptimization
 from swarms_torch.queen_bee import QueenBeeGa
 from swarms_torch.spiral_optimization import SPO
 
+from swarms_torch.cen import CellularSwarm
+from swarms_torch.nnt import NNTransformer
+
 __all__ = [
     "ParticleSwarmOptimization",
     "AntColonyOptimization",
---
+++ swarms_torch/cen.py
@@ -0,0 +1,162 @@
+# import torch 
+# from torch import nn
+
+# class TransformerCell(nn.Module):
+#     def __init__(
+#         self,
+#         input_dim,
+#         nhead,
+#         num_layers=1,
+#     ):
+#         super(TransformerCell, self).__init__()
+#         self.transformer = nn.Transformer(
+#             input_dim,
+#             nhead=nhead,
+#             num_encoder_layers=num_layers
+#         )
+    
+#     def forward(self, x):
+#         return self.transformer(x, x)
+    
+
+
+
+# class CellularTransformerNetwork(nn.Module):
+#     """
+#     Cellular Transformer Network
+#     ----------------------------
+
+#     Architecture:
+#         - Input -> TransformerCell -> TransformerCell -> ... -> Output
+
+#     Overview:
+#         This is a cellular neural network that uses a transformer cell
+#         to process the input.
+    
+#     Args:
+#         cell_count (int): Number of transformer cells
+#         input_dim (int): Input dimension
+#         nhead (int): Number of heads in the transformer cell
+    
+#     Returns:
+#         torch.Tensor: Output tensor
+    
+#     Usage:
+#         >>> x = torch.randn(10, 32, 512)
+#         >>> model = CellularTransformerNetwork(cell_count=5, input_dim=512, nhead=8)
+#         >>> output = model(x)
+#         >>> print(output)
+
+#     """
+#     def __init__(
+#         self,
+#         cell_count,
+#         input_dim,
+#         nhead,
+#     ):
+#         super(CellularTransformerNetwork, self).__init__()
+#         self.cells = nn.ModuleList([
+#             TransformerCell(input_dim, nhead) for _ in range(cell_count)
+#         ])
+
+#     def forward(self, x):
+#         for cell in self.cells:
+#             x = cell(x)
+#         return x
+    
+# x = torch.randn(10, 32, 512)
+# model = CellularTransformerNetwork(cell_count=5, input_dim=512, nhead=8)
+# output = model(x)
+# print(output)
+
+import torch 
+from torch import nn
+
+class TransformerCell(nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        nhead,
+        num_layers=1,
+        neighborhood_size=3,
+    ):
+        super(TransformerCell, self).__init__()
+        self.transformer = nn.Transformer(
+            input_dim,
+            nhead=nhead,
+            num_encoder_layers=num_layers
+        )
+        self.neighborhood_size = neighborhood_size
+    
+    def forward(self, x, neigbors):
+        x = self.transformer(x, x)
+        out = torch.cat([x] + neigbors, dim=0)
+        print(out)
+        return out
+    
+
+class CellularSwarm(nn.Module):
+    """
+    CellularSwarm
+
+    Architecture:
+        - Input -> TransformerCell -> TransformerCell -> ... -> Output
+
+    Overview:
+    CellularSwarm is a cellular neural network that uses a transformer cell
+    to process the input.
+
+    Args:
+        cell_count (int): Number of transformer cells
+        input_dim (int): Input dimension
+        nhead (int): Number of heads in the transformer cell
+        time_steps (int): Number of time steps to run the network
+    
+    Returns:
+        torch.Tensor: Output tensor
+    
+    Usage:
+        >>> x = torch.randn(10, 32, 512)
+        >>> model = CellularSwarm(cell_count=5, input_dim=512, nhead=8)
+        >>> output = model(x)
+        >>> print(output)
+    
+    
+    """
+    def __init__(
+        self,
+        cell_count,
+        input_dim,
+        nhead,
+        time_steps=4
+    ):
+        super(CellularSwarm, self).__init__()
+        self.cells = nn.ModuleList([
+            TransformerCell(input_dim, nhead) for _ in range(cell_count)
+        ])
+        self.time_steps = time_steps
+    
+    def forward(self, x):
+        for _ in range(self.time_steps):
+            for i, cell in enumerate(self.cells):
+                #get neighboring cells states
+                start_idx = max(
+                    0,
+                    i - cell.neighborhood_size
+                )
+
+                end_idx = min(
+                    len(self.cells), i + cell.neighborhood_size + 1
+                )
+
+                neighbors = [
+                    self.cells[j].transformer(x, x) for j in range(start_idx, end_idx) if j != i
+                ]
+
+                x = cell(x, neighbors)
+        return x
+
+# Example usage:
+input_sequence = torch.randn(10, 32, 512)  # sequence length of 10, batch size of 32, embedding size of 512
+model = CellularSwarm(cell_count=5, input_dim=512, nhead=8)
+output = model(input_sequence)
