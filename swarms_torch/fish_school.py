2122f4dfcf91e9b0ca656f793d457fd4fa8db9fa
Kye <kye@apacmediasolutions.com>
2023-10-11 19:07:18 -0400

    fish school methods

--- swarms_torch/fish_school.py
+++ swarms_torch/fish_school.py
@@ -1,6 +1,8 @@
+import torch
 from torch import nn
 from torch.nn import CrossEntropyLoss, Transformer
 from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class Fish(nn.Module):
@@ -63,22 +65,73 @@ class Fish(nn.Module):
         dim,
         heads,
         depth,
+        dynamic_learning_rate=False,
+        early_stopping=False,
+        complexity_regularization=False,
     ):
         super().__init__()
+        self.optimizer = Adam(self.parameters())
+
+        if self.dynamic_learning_rate:
+            self.scheduler = ReduceLROnPlateau(self.optimizer, "min")
+
         self.model = Transformer(
             d_model=dim, nhead=heads, num_encoder_layers=depth, num_decoder_layers=depth
         )
         self.food = 0
 
+        if self.early_stopping:
+            self.best_food = float("inf")
+            self.patience = 0
+
     def train(self, src, tgt, labels):
         """Trains the fish school"""
         outputs = self.model(src, tgt)
+
         loss = CrossEntropyLoss()
         loss = loss(outputs.view(-1, outputs.size(-1)), labels.view(-1))
+
+        if self.complexity_regularization:
+            loss = loss + alpha * sum(p.pow(2.0).sum() for p in self.model.parameters())
+
         loss.backward()
-        forward = Adam(self.model.parameters())
-        forward.step()
+
+        if self.dynamic_learning_rate:
+            self.scheduler.step(loss)
+        else:
+            forward = self.optimizer
+            forward.step()
+
         self.food = -loss.item()  # use negative loss as food
+
+    def forward(self, src, tgt):
+        """Forward pass of the fish school"""
+        return self.model(src, tgt)
+
+    def early_stopping(self, food):
+        """
+        Early stopping if the fish is not improving.
+        """
+        if food < self.best_food:
+            self.best_food = food
+            self.patience = 0
+        else:
+            self.patience += 1
+            if self.patience > 5:
+                return True
+        return False
+
+    def generate(self, src, tgt):
+        """
+        Generate a sequence using the fish's model.
+        """
+        return self.model.generate(src, tgt)
+
+    def save(self, path):
+        """
+        Save the fish's model.
+        """
+        torch.save(self.model.state_dict(), path)
 
 
 class FishSchool(nn.Module):
@@ -124,3 +177,87 @@ class FishSchool(nn.Module):
                     # transformer weights from the best performing fish
                     best_fish = max(self.fish, key=lambda f: f.food)
                     fish.model.load_state_dict(best_fish.model.state_dict())
+
+    def generate(self, src, tgt):
+        """
+        Generate a sequence using the fish school's model.
+        """
+        return self.fish[0].generate(src, tgt)
+    
+    def predict(self, src, tgt):
+        """
+        Ensemble learning => enseble prediction of top performing models
+
+        averages outputs of the top peforming models
+        """
+        top_fish = sorted(
+            self.fish,
+            key=lambda f: f.food, 
+            reverse=True
+        )[:num_top_fish]
+        outputs = [fish.model(src, tgt) for fish in top_fish]
+        return sum(outputs) / len(outputs)
+    
+    def save(self, path):
+        """
+        Save the fish school's models.
+        """
+        for i, fish in enumerate(self.fish):
+            fish.save(path + f"fish_{i}.pt")
+        
+    def load(self, path):
+        """
+        Load the fish school's models.
+        """
+        for i, fish in enumerate(self.fish):
+            fish.model.load_state_dict(torch.load(path + f"fish_{i}.pt"))
+    
+    def early_stopping(self):
+        """
+        Early stopping if the fish school is not improving.
+        """
+        for fish in self.fish:
+            if fish.early_stopping(fish.food):
+                return True
+        return False
+    
+    def dynamic_learning_rate(self):
+        """
+        Dynamic learning rate for the fish school.
+        """
+        for fish in self.fish:
+            fish.dynamic_learning_rate = True
+    
+    def complexity_regularization(self):
+        """
+        Complexity regularization for the fish school.
+        """
+        for fish in self.fish:
+            fish.complexity_regularization = True
+
+    def reset(self):
+        """
+        Reset the fish school's food.
+        """
+        for fish in self.fish:
+            fish.food = 0
+    
+    def __getitem__(self, index):
+        """Get the fish at the given index"""
+        return self.fish[index]
+    
+    def __len__(self):
+        """Get the number of fish in the school"""
+        return len(self.fish)
+    
+    def __iter__(self):
+        """Iterate over the fish in the school"""
+        return iter(self.fish)
+    
+    def __next__(self):
+        """Get the next fish in the school"""
+        return next(self.fish)
+    
+    def __str__(self):
+        """Get the string representation of the fish school"""
+        return str(self.fish)
--- tests/fish_school.py
+++ tests/fish_school.py
@@ -2,9 +2,11 @@ import pytest
 import torch
 from swarms_torch.fish_school import Fish, FishSchool
 
+
 def test_fish_initialization():
     fish = Fish(dim=512, heads=8, depth=6)
     assert isinstance(fish, Fish)
+
 
 def test_fish_train():
     fish = Fish(dim=512, heads=8, depth=6)
@@ -14,10 +16,12 @@ def test_fish_train():
     fish.train(src, tgt, labels)
     assert isinstance(fish.food, float)
 
+
 def test_fishschool_initialization():
     fishschool = FishSchool(num_fish=10, dim=512, heads=8, depth=6, num_iter=100)
     assert isinstance(fishschool, FishSchool)
     assert len(fishschool.fish) == 10
+
 
 def test_fishschool_forward():
     fishschool = FishSchool(num_fish=10, dim=512, heads=8, depth=6, num_iter=100)
