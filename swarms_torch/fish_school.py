f281e4be5e5fc5bc16bc5049be31efd5cc8dd51c
Kye <kye@apacmediasolutions.com>
2023-10-10 21:13:34 -0400

    fish school clean up

--- swarms_torch/fish_school.py
+++ swarms_torch/fish_school.py
@@ -36,6 +36,27 @@ class Fish(nn.Module):
     >>> fish.food
     -0.123456789
 
+
+
+    Example2
+    # Create random source and target sequences
+    src = torch.randn(10, 32, 512)
+    tgt = torch.randn(10, 32, 512)
+
+    # Create random labels
+    labels = torch.randint(0, 512, (10, 32))
+
+    # Create a fish and train it on the random data
+    fish = Fish(512, 8, 6)
+    fish.train(src, tgt, labels)
+    print(fish.food)  # Print the fish's food
+
+    # Create a fish school and optimize it on the random data
+    school = FishSchool(10, 512, 8, 6, 100)
+    school.forward(src, tgt, labels)
+    print(school.fish[0].food)  # Print the first fish's food
+
+
     """
 
     def __init__(
@@ -104,21 +125,3 @@ class FishSchool(nn.Module):
                     # transformer weights from the best performing fish
                     best_fish = max(self.fish, key=lambda f: f.food)
                     fish.model.load_state_dict(best_fish.model.state_dict())
-
-
-# Create random source and target sequences
-src = torch.randn(10, 32, 512)
-tgt = torch.randn(10, 32, 512)
-
-# Create random labels
-labels = torch.randint(0, 512, (10, 32))
-
-# Create a fish and train it on the random data
-fish = Fish(512, 8, 6)
-fish.train(src, tgt, labels)
-print(fish.food)  # Print the fish's food
-
-# Create a fish school and optimize it on the random data
-school = FishSchool(10, 512, 8, 6, 100)
-school.forward(src, tgt, labels)
-print(school.fish[0].food)  # Print the first fish's food
---
+++ tests/fish_school.py
@@ -0,0 +1,28 @@
+import pytest
+import torch
+from swarms_torch.fish_school import Fish, FishSchool
+
+def test_fish_initialization():
+    fish = Fish(dim=512, heads=8, depth=6)
+    assert isinstance(fish, Fish)
+
+def test_fish_train():
+    fish = Fish(dim=512, heads=8, depth=6)
+    src = torch.randn(10, 32, 512)
+    tgt = torch.randn(10, 32, 512)
+    labels = torch.randint(0, 512, (10, 32))
+    fish.train(src, tgt, labels)
+    assert isinstance(fish.food, float)
+
+def test_fishschool_initialization():
+    fishschool = FishSchool(num_fish=10, dim=512, heads=8, depth=6, num_iter=100)
+    assert isinstance(fishschool, FishSchool)
+    assert len(fishschool.fish) == 10
+
+def test_fishschool_forward():
+    fishschool = FishSchool(num_fish=10, dim=512, heads=8, depth=6, num_iter=100)
+    src = torch.randn(10, 32, 512)
+    tgt = torch.randn(10, 32, 512)
+    labels = torch.randint(0, 512, (10, 32))
+    fishschool.forward(src, tgt, labels)
+    assert isinstance(fishschool.fish[0].food, float)
