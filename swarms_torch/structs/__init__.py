92976a209d2b9a60096d9323b9fb6fd4bd9c7042
Kye <kye@apacmediasolutions.com>
2024-01-20 20:47:13 -0500

    [FEATS] [Structs]

--- swarms_torch/structs/__init__.py
+++ swarms_torch/structs/__init__.py
@@ -1,5 +1,11 @@
 from swarms_torch.structs.parallel_wrapper import ParallelSwarm
 from swarms_torch.structs.moe import SwitchGate, SwitchMoE
-from swarms_torch.structs.simple_moe import SimpleMoE
+from swarms_torch.structs.simple_moe import GatingMechanism, SimpleMoE
 
-__all__ = ["ParallelSwarm", "SwitchGate", "SwitchMoE", "SimpleMoE"]
+__all__ = [
+    "ParallelSwarm",
+    "SwitchGate",
+    "SwitchMoE",
+    "GatingMechanism",
+    "SimpleMoE",
+]
