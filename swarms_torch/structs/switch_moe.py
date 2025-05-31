38a53fc7e2e30be53dd8b5786a802a4028d011bf
Kye Gomez <kye@swarms.world>
2025-05-31 02:36:14 -0700

    cleanup

--- swarms_torch/structs/switch_moe.py
+++ swarms_torch/structs/switch_moe.py
@@ -1,7 +1,41 @@
 import torch
 from torch import nn, Tensor
 import torch.nn.functional as F
-from zeta.nn import FeedForward
+
+
+class FeedForward(nn.Module):
+    """
+    Simple FeedForward module.
+    
+    Args:
+        dim (int): Input dimension
+        hidden_dim (int): Hidden dimension
+        mult (int): Multiplier for hidden dimension
+        dropout (float): Dropout rate
+    """
+    
+    def __init__(
+        self,
+        dim: int,
+        hidden_dim: int = None,
+        mult: int = 4,
+        dropout: float = 0.0,
+        *args,
+        **kwargs,
+    ):
+        super().__init__()
+        hidden_dim = hidden_dim or dim * mult
+        
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout),
+        )
+    
+    def forward(self, x: Tensor) -> Tensor:
+        return self.net(x)
 
 
 class SwitchGate(nn.Module):
